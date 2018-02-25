c11937844fb2c052a5fda2d981176fc37f528558
nicoalli <alli.nicolette@gmail.com>
2018-02-24 19:45:59 -0500

    created react app and file structure

---
+++ sap/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+registerServiceWorker();
