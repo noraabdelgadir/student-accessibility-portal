79d3951cf34e19b808f4d7e237503c69967e4ac3
nicoalli <alli.nicolette@gmail.com>
2018-02-28 15:21:24 -0500

    fixed the graph and set it up to serve static files

--- SAP/index.js
+++ SAP/index.js
@@ -2,14 +2,23 @@
 
 //setup all the variables needed
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var server = require('http').Server(app);
 
-app.get('/', (req, res) => {
-  res.send('NOTHING HERE YET')
-});
+//serve as static files for now
+app.use('/views',express.static(path.join(__dirname, '/views')));
+app.use('/assets',express.static(path.join(__dirname, '/assets')));
 
-var server = app.listen(3000, function() {
+//serve directory for assets
+//app.use(express.static(__dirname + '/assets'));
+//app.use(express.static(__dirname + '/'));
+
+/*app.get('/', (req, res) => {
+  res.send('NOTHING HERE YET')
+});*/
+
+var server = app.listen(3030, function() {
   console.log('Running on 127.0.0.1:%s', server.address().port);
 });
