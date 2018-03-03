6903f76a9d0f2c8beead95631aa1c1a290f16114
Nora Abdelgadir <nora.abdelgadir@mail.utoronto.ca>
2018-03-03 18:42:41 -0500

    added navbar and structure for page

--- SAP/views/CategoryGraph/categories.js
+++ SAP/views/CategoryGraph/categories.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   var sys = arbor.ParticleSystem(1000, 400, 1);
-  sys.parameters({gravity:true});
-  sys.renderer = Renderer("#viewport") ;
+  sys.parameters({gravity: true});
+  sys.renderer = Renderer("#viewport");
+
+  // sample categories and its services
   var data = {
      nodes : {
-       category: {'color': '#002A5C', 'shape': 'dot', 'label': 'category'},
+       category: {'mass':'1', 'color': '#002A5C', 'shape': 'dot', 'label': 'category'},
        service1: {'color': '#008BB0', 'shape': 'square', 'label': 'service1'},
        service2:{'color': '#008BB0', 'shape': 'square', 'label': 'service2'},
        service3:{'color': '#008BB0', 'shape': 'square', 'label': 'service3'},
@@ -13,28 +15,9 @@ $(document).ready(function(){
        service6:{'color': '#008BB0', 'shape': 'square', 'label': 'service6'}
      },
      edges:{
-       category: {service1:{}, service2:{}, service3:{}, service4:{},
-                        service5:{}, service6:{}}
+       category: {service1:{length:0.4}, service2:{length:0.4}, service3:{length:0.4}, 
+              service4:{length:0.4}, service5:{length:0.4}, service6:{length:0.4}}
      }
    };
   sys.graft(data);
-
-  $("canvas").mousedown(function(e) {
-      var pos = $(this).offset();
-      var p = {x:e.pageX-pos.left, y:e.pageY-pos.top}
-      selected = nearest = dragged = sys.nearest(p);
-
-      if (selected.node == "service1") { // doesn't work
-        var s1 = {
-          nodes : {
-            description:{'color': '#000000', 'shape': 'square', 'label': 'description'}
-          },
-          edges :{
-            service1: {description:{}}
-          }
-        };
-        sys.graft(s1);
-      }
-      return false;
-  });
 });
