8b980d98592b2c62d605f5c54a9a82d097ec0a3e
Kantawan Charoenphiphob <c.kantawan@gmail.com>
2018-03-05 02:21:16 -0500

    login and sign up working only have to work on style now

--- SAP/index.js
+++ SAP/index.js
@@ -22,3 +22,25 @@ app.use('/assets',express.static(path.join(__dirname, '/assets')));
 var server = app.listen(3030, function() {
   console.log('Running on 127.0.0.1:%s', server.address().port);
 });
+
+// meta data
+var datas = {"guest": {"password" : "guest", "firstname" : "guest", "lastname": "guest"}}
+app.get('/all', sendAll);
+
+app.get('/add/:user/:pw/:first/:last', addUser);
+
+function sendAll(request, response) {
+  response.send(datas);
+}
+
+function addUser(request, response){
+  var username = request.params.user;
+  var password = request.params.pw;
+  var first = request.params.first;
+  var last = request.params.last;
+
+  var newInput = {"password": password, "firstname": first, "lastname": last}
+
+  datas[username] = newInput
+
+}
