fd07363902ae848b0abc5e9f6a248a481acf048e
Kantawan Charoenphiphob <c.kantawan@gmail.com>
2018-03-21 19:23:17 -0400

    login

--- SAP/views/Login/login.js
+++ SAP/views/Login/login.js
@@ -1,7 +1,7 @@
 function checkLogin(form){
 
   var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", 'http://127.0.0.1:3030/all', false);
+  rawFile.open("GET", 'http://localhost:3030/user', false);
   rawFile.onreadystatechange = function ()
   {
       if(rawFile.readyState === 4) {
@@ -23,14 +23,32 @@ function checkLogin(form){
 
   // checking password goes here now
 
-  if(obj.hasOwnProperty(form.username.value)){
-    if (obj[form.username.value]["password"] == form.password.value){
-      flag = true;
-      window.open('../PersonalGraph/main.html',"_self")
+  // if(obj.hasOwnProperty(form.username.value)){
+  //   if (obj[form.username.value]["password"] == form.password.value){
+  //     flag = true;
+  //     window.open('../PersonalGraph/main.html',"_self")
+  //   }
+  // }
+
+  for(var user in obj){
+    console.log("here");
+    console.log(obj[user]['utorid']);
+    console.log(obj[user]['password']);
+
+    if (obj[user]['utorid'] == form.username.value){
+      if(form.password.value == obj[user]['password']){
+        flag = true;
+
+        var userStr = "/curUser/" + form.username.value;
+
+        var pop = window.open(userStr, "Popup", "height=1, width=1, status=no, toolbar=no, menubar=no, location=no, top = 100000, left=100000 ");
+        pop.close();
+
+
+        window.open('../PersonalGraph/main.html',"_self")
+      }
     }
   }
-
-
 
   // for (var i = 0, len = obj.length; i < len; ++i) {
   //   var user = obj[i].userName;
