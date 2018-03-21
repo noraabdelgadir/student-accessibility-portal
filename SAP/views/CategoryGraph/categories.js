1d6b8d7c83f471c7f4656dab84ff019d6db9f6f2
Nora Abdelgadir <nora.abdelgadir@mail.utoronto.ca>
2018-03-21 18:38:46 -0400

    added descriptions from txt files

--- SAP/views/CategoryGraph/categories.js
+++ SAP/views/CategoryGraph/categories.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var sys = arbor.ParticleSystem(10000, 400, 1);
   sys.parameters({repulsion: 10000, gravity: false, dt: 0.35});
   sys.renderer = Renderer("#viewport");
+
+  // loading the descriptions of the services
+
+  // Documents
+  $("#letterOfAccommodation").load("../../txtFiles/letterOfAccommodation.txt");
+  $("#verificationOfIllness").load("../../txtFiles/verificationOfIllness.txt");
+  // $("#saved").load("../../txtFiles/.txt");
+
+  // Notes
+  $("#volunteer").load("../../txtFiles/volunteer.txt");
+  $("#uploadNotes").load("../../txtFiles/uploadNotes.txt");
+  $("#requestNotetaking").load("../../txtFiles/requestNotetaking.txt");
+
+  // Counsellors
+  $("#adaptiveTechnologist").load("../../txtFiles/adaptiveTechnologist.txt");
+  $("#learningStrategist").load("../../txtFiles/learningStrategist.txt");
+  $("#accessibilityAdvisors").load("../../txtFiles/accessibilityAdvisor.txt");
+
+  // Test and Exam Accommodation
+  // $("#").load("../../txtFiles/accommodationRenewal.txt");
+  
+  // $("#").load("../../txtFiles/.txt");
 
   // adopted from http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
   function GetURLParameter(sParam){
@@ -21,11 +43,11 @@ $(document).ready(function(){
          nodes : {
            middleNode: {'mass':'1', 'color': '#002A5C', 'shape': 'dot', 'label': 'Documents'},
            verificationOfIllness: {'color': '#008BB0', 'shape': 'square', 'label': 'Verification of Illness'},
-           letterOfAccomodation:{'color': '#008BB0', 'shape': 'square', 'label': 'Letter of Accomodation'},
+           letterOfAccommodation:{'color': '#008BB0', 'shape': 'square', 'label': 'Letter of Accomodation'},
            saved:{'color': '#008BB0', 'shape': 'square', 'label': 'Saved'},
          },
          edges:{
-           middleNode: {verificationOfIllness:{length:0.4}, letterOfAccomodation:{length:0.4},
+           middleNode: {verificationOfIllness:{length:0.4}, letterOfAccommodation:{length:0.4},
                   saved:{length:0.4}}
          }
        };
