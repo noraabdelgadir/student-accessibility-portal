5df8d59b9250cbc6ac030f9d582f957a2ebfe41d
bcrisostomo <bcrisostomo@hotmail.com>
2018-03-22 11:30:38 -0400

    Changed the linked path for the nav bar for all pages

--- SAP/controllers/services.controller.js
+++ SAP/controllers/services.controller.js
@@ -2,7 +2,7 @@ var Users = require('../models/User');
 var Graphs = require('../models/Graph');
 
 function loadServices (req, res, next){
-    res.sendFile('/SAP/views/AllServices/allServices.html', {'root': '../'})
+    res.sendFile('./SAP/views/AllServicesGraph/allServices.html', {'root': '../'})
 }
 
 module.exports = {loadServices};
