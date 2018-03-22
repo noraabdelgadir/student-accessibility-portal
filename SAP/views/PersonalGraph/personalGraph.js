41642126ed2a94e2724631aa1017ff4f94b79ea6
Kantawan Charoenphiphob <c.kantawan@gmail.com>
2018-03-22 01:24:40 -0400

    formatting for the catergories graphs

--- SAP/views/PersonalGraph/personalGraph.js
+++ SAP/views/PersonalGraph/personalGraph.js
@@ -25,8 +25,8 @@ $(document).ready(function(){
         nodes:{
           center:{'mass':'.1','color':'#00215C','shape':'dot','label':curUser},
           cat1:{'mass':'1','color':'#008BB0','shape':'dot','label':'Counsellors','link':'http://localhost:3030/views/CategoryGraph/categories.html?category=counsellors'},
-          cat2:{'mass':'1','color':'#008BB0','shape':'dot','label':'Test and Exam Accomodation'},
-          cat3:{'mass':'1','color':'#008BB0','shape':'dot','label':'Note Taking'}
+          cat2:{'mass':'1','color':'#008BB0','shape':'dot','label':'Test and Exam Accomodation','link':'http://localhost:3030/views/CategoryGraph/categories.html?category=tests'},
+          cat3:{'mass':'1','color':'#008BB0','shape':'dot','label':'Note Taking','link':'http://localhost:3030/views/CategoryGraph/categories.html?category=notes'},
         },
         edges:{
           center:{ cat1:{length:0.4}, cat2:{}, cat3:{length:0.4} }}
