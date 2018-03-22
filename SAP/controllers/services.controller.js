6dae025987ea43a2ffa031f4bf09108371b6cb61
bcrisostomo <bcrisostomo@hotmail.com>
2018-03-22 12:39:59 -0400

    Resolved conflict

--- SAP/controllers/services.controller.js
+++ SAP/controllers/services.controller.js
@@ -2,8 +2,10 @@ var Users = require('../models/User');
 var Graphs = require('../models/Graph');
 
 function loadServices (req, res, next){
-    res.sendFile('./SAP/views/AllServicesGraph/allServices.html', {'root': '../'})
+
     res.sendFile('/SAP/views/AllServicesGraph/allServices.html', {'root': '../'})
+
+
 }
 
 module.exports = {loadServices};
