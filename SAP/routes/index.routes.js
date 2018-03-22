e03e08ab37f199c552d165f28ce57d9d1b23a4b8
Nora Abdelgadir <nora.abdelgadir@mail.utoronto.ca>
2018-03-22 12:50:34 -0400

    added category route

--- SAP/routes/index.routes.js
+++ SAP/routes/index.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 //Set up variables to hold our routes
 const userRoutes = require('./user.route');
 const mainRoutes = require('./main.route');
+const categoryRoutes = require('./category.route');
 
 //Mount all routes to /api
 router.use('/user', userRoutes);
