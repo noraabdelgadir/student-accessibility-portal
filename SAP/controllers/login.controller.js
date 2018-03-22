8eb6e39402703deeedc51b4681f1fb8a094eb812
Kantawan Charoenphiphob <c.kantawan@gmail.com>
2018-03-22 13:22:20 -0400

    rerouting login and signup now /index html right after local host should work

--- SAP/controllers/login.controller.js
+++ SAP/controllers/login.controller.js
@@ -9,4 +9,12 @@ function loadCSS (req, res, next){
   res.sendFile('/SAP/views/Login/index.css', {'root': '../'})
 }
 
-module.exports = {loadIndex, loadCSS};
+function loadSignup(req, res, next){
+  res.sendFile('/SAP/views/Login/signup.html', {'root': '../'})
+}
+
+function loadSignupCSS(req, res, next){
+  res.sendFile('/SAP/views/Login/signup.css', {'root': '../'})
+}
+
+module.exports = {loadIndex, loadCSS, loadSignup, loadSignupCSS};
--- SAP/routes/login.route.js
+++ SAP/routes/login.route.js
@@ -4,8 +4,12 @@ const mainCtrl = require('../controllers/login.controller');
 const router = express.Router();
 
 // Render main page
-router.get('/', mainCtrl.loadIndex);
+router.get('/index.html', mainCtrl.loadIndex);
 
 router.get('/index.css', mainCtrl.loadCSS);
+
+router.get('/signup.css', mainCtrl.loadSignupCSS);
+
+router.get('/signup.html', mainCtrl.loadSignup);
 
 module.exports = router;
