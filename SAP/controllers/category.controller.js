32ab9aa5ab7aa33bf6ffdb9a67ab416026a5bba3
Ruotai Bai <andre.bai@mail.utoronto.ca>
2018-03-26 23:35:22 -0400

    categories

--- SAP/controllers/category.controller.js
+++ SAP/controllers/category.controller.js
@@ -5,13 +5,34 @@ function loadCategory (req, res, next){
 }
 
 /** Find specific category **/
-function findCategory(req, res, next){
-    Graph.find({name: req.params.name},function(err, services) {
+ffunction findCategory(req, res, next){
+  var realName = ''
+  if (req.params.name == "documents"){
+    realName = 'Documents';
+  }
+  else if (req.params.name == "counsellors"){
+    realName = "Counsellors";
+  }
+  else if (req.params.name == "notes"){
+    realName = "Note Taking";
+  }
+  else if (req.params.name == "test"){
+    realName = "Test and Exam Accommodation";
+  }
+  else if (req.params.name == "build"){
+    realName = "Building Accessibility";
+  }
+
+  console.log(realName)
+    Graph.findOne({name: realName},function(err, services) {
           if (err) throw err;
-          res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030/category');
-          res.setHeader('Access-Control-Allow-Methods', 'GET');
-          res.json(services);
+
+          // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030/category');
+          // res.setHeader('Access-Control-Allow-Methods', 'GET');
+          // res.json(services);
+          res.send(services);
     });
 }
 
 module.exports = {loadCategory, findCategory};
+
